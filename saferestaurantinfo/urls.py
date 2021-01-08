b04fcf4ae63a078f914230bf7f51497527b3c993
ssac-academy <ssac-academy@example.com>
2021-01-08 09:13:18 +0900

    modify files

--- saferestaurantinfo/urls.py
+++ saferestaurantinfo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 
-from .views import HomeView, RestaurantListView, SafeRestaurantInfo
+from .views import HomeView, RestaurantListView, SafeRestaurantInfo, SafeRestaurantDetailView
 
 app_name = 'saferestaurantinfo'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name="index"), # home 연결
     path('saferestaurantlist/', RestaurantListView.as_view(), name="saferestaurantlist"),
     path('saferestaurantlist/<str:region>/', SafeRestaurantInfo.as_view(), name='saferestaurant_region_info'),
+    path('<str:pk>/', SafeRestaurantDetailView.as_view(), name="saferestaurant_detail"),
+
 ]
--- saferestaurantinfo/views.py
+++ saferestaurantinfo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, View
+from django.views.generic.list import ListView
 import pymysql
 import json
 from datetime import date, timedelta
@@ -11,9 +12,14 @@ class HomeView(TemplateView):
 
 class RestaurantListView(TemplateView):
     template_name = 'saferestaurantinfo/saferestaurantlist.html'
+    
+
+
+
 
 class SafeRestaurantInfo(View):
     def get(self, request, region):
+
         connection_info = { 'host': 'localhost', 'user': 'root', 'password': 'Kweondh123!', 'db': 'saferestaurant', 'charset': 'utf8' }
         conn = pymysql.connect(**connection_info)
         cursor = conn.cursor()
@@ -32,4 +38,23 @@ class SafeRestaurantInfo(View):
         
         json_data=json.dumps(modified_data, ensure_ascii=False)
 
+
         return HttpResponse(json_data, content_type="application/json;charset=utf-8")
+    
+class SafeRestaurantDetailView(View):
+    def get(self, request, pk):
+
+        connection_info = { 'host': 'localhost', 'user': 'root', 'password': 'Kweondh123!', 'db': 'saferestaurant', 'charset': 'utf8' }
+        conn2 = pymysql.connect(**connection_info)
+        cursor2 = conn2.cursor()
+
+        sql2 = 'SELECT * from safe_restaurant where seq=' + pk
+        cursor2.execute(sql2)
+        conn2.commit()
+        datas2=[]
+        modified_data2=[]
+        datas2 = cursor2.fetchall()
+        modified_data2 = (datas2[0][0],datas2[0][2]+' '+datas2[0][3],datas2[0][4],datas2[0][5],datas2[0][6].isoformat(),datas2[0][7]+' '+datas2[0][8],datas2[0][9],datas2[0][10],datas2[0][11])
+        json_data2=json.dumps(modified_data2, ensure_ascii=False)
+        
+        return HttpResponse(json_data2, content_type="application/json;charset=utf-8")
